3df4b8e2afc8492be33b418a8145e160b1443aac
RuiT8 <khader.mallouh@gmail.com>
2023-08-01 11:51:02 +0300

    .

--- app/Http/Controllers/Admin/RoleCrudController.php
+++ app/Http/Controllers/Admin/RoleCrudController.php
@@ -65,25 +65,7 @@ class RoleCrudController extends CrudController
          * - CRUD::field('price')->type('number');
          */
 
-         CRUD::field([  // Select
-            'label'     => "Role",
-            'type'      => 'select',
-            'name'      => 'role_id', // the db column for the foreign key
 
-            // optional
-            // 'entity' should point to the method that defines the relationship in your Model
-            // defining entity will make Backpack guess 'model' and 'attribute'
-            'entity'    => 'role',
-
-            // optional - manually specify the related model and attribute
-            'model'     => "App\Models\Role", // related model
-            'attribute' => 'name', // foreign key attribute that is shown to user
-
-            // optional - force the related options to be a custom query, instead of all();
-            'options'   => (function ($query) {
-                 return $query->orderBy('name', 'ASC')->where('depth', 1)->get();
-             }), //  you can use this to filter the results show in the select
-         ]);
     }
 
     /**
