36eb4904fad0e9c5063e6a8bd6130d16eb71a8b9
RuiT8 <khader.mallouh@gmail.com>
2023-08-03 15:05:28 +0300

    .

---
+++ app/Http/Controllers/Api/Auth/AppleController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AppleController extends Controller
+{
+    public function oAuth(Request $request){
+        // TODO: Apple OAuth functionalities
+    }
+}
---
+++ app/Http/Controllers/Api/Auth/SocialiteController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SocialiteController extends Controller
+{
+
+    public function oAuth(Request $request, String $provider){
+        // TODO: Google and Facebook OAuth functionalities
+    }
+
+}
