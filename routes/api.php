c9fd7e9ac76a4bb6691dd3a606e01c1b0a90735a
RuiT8 <khader.mallouh@gmail.com>
2023-08-09 19:43:42 +0300

    .

--- routes/api.php
+++ routes/api.php
@@ -17,9 +17,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 
 Route::prefix('v1')->group(function () {
@@ -35,4 +32,8 @@ Route::prefix('v1')->group(function () {
 
     });
 
+
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
