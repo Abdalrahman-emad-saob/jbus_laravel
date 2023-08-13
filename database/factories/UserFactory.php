b9c9cdf99f5e45707a9d02980868f11d14f67daf
Khader Mallouh <khader.mallouh@gmail.com>
2023-08-13 17:34:10 +0300

    .

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,7 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'role' => Arr::random([User::$admin, User::$driver, User::$passenger]),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('password'), // password
             'remember_token' => Str::random(10),
         ];
     }
