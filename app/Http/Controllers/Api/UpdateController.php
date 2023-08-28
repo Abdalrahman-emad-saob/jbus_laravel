50a98784aaf55eab0c17c2a07e434d6df32b4f6b
Fadl masri <fdlmasri@gmail.com>
2023-08-28 13:24:06 +0300

    using user email instead of userid for pfp update

--- app/Http/Controllers/Api/UpdateController.php
+++ app/Http/Controllers/Api/UpdateController.php
@@ -17,43 +17,42 @@ class UpdateController extends Controller
 {
 
     public function updatePFP(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'user_id' => 'required|exists:users,id',
-            'profile_picture' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048', // Adjust max file size as needed
-        ]);
+{
+    $validator = Validator::make($request->all(), [
+        'email' => 'required|email|exists:users,email', // Change the validation rule
+        'profile_picture' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+    ]);
 
-        if ($validator->fails()) {
-            return response()->json(['errors' => $validator->errors()], 400);
-        }
-
-        $user = User::find($request->user_id);
-
-        if (!$user) {
-            return response()->json(['message' => 'User not found'], 404);
-        }
-
-        $profilePicture = $request->file('profile_picture');
-        $imageName = time() . '.' . $profilePicture->getClientOriginalExtension();
-        $imagePath = 'storage/' . $imageName;
-
-        // Save the image
-        Storage::putFileAs('public', $profilePicture, $imageName);
-
-        // Update the profile picture in the passenger_profiles table if it exists
-        $passengerProfile = PassengerProfile::where('passenger_id', $user->id)->first();
-        if ($passengerProfile) {
-            $passengerProfile->picture = $imagePath;
-            $passengerProfile->save();
-        } else {
-            $passengerProfile = new PassengerProfile();
-            $passengerProfile->passenger_id = $user->id;
-            $passengerProfile->picture = $imagePath;
-            $passengerProfile->save();
-        }
-
-        return response()->json(['message' => 'Image updated'], 200);
+    if ($validator->fails()) {
+        return response()->json(['errors' => $validator->errors()], 400);
     }
+
+    $user = User::where('email', $request->email)->first(); // Find the user by email
+
+    if (!$user) {
+        return response()->json(['message' => 'User not found'], 404);
+    }
+
+    $profilePicture = $request->file('profile_picture');
+    $imageName = time() . '.' . $profilePicture->getClientOriginalExtension();
+    $imagePath = 'storage/' . $imageName;
+
+    Storage::putFileAs('public', $profilePicture, $imageName);
+
+    $passengerProfile = PassengerProfile::where('passenger_id', $user->id)->first();
+    if ($passengerProfile) {
+        $passengerProfile->picture = $imagePath;
+        $passengerProfile->save();
+    } else {
+        $passengerProfile = new PassengerProfile();
+        $passengerProfile->passenger_id = $user->id;
+        $passengerProfile->picture = $imagePath;
+        $passengerProfile->save();
+    }
+
+    return response()->json(['message' => 'Image updated'], 200);
+}
+
 
 
 
