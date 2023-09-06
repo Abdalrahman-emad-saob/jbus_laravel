11f233a609a943db0f45999e66424904cbff51a0
Abdalrahman-emad-saob <81173173+Abdalrahman-emad-saob@users.noreply.github.com>
2023-09-06 22:07:40 +0300

    .

--- app/Http/Controllers/Api/InterestRoutesController.php
+++ app/Http/Controllers/Api/InterestRoutesController.php
@@ -7,14 +7,16 @@ use App\Models\InterestPoint;
 use App\Models\Point;
 use App\Models\Route;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class InterestRoutesController extends Controller
 {
+    // TODO
     public function interestRoutes(Request $request)
     {
-        $passengerId = $request->passenger_id;
+        $passengerId = Auth::id();
         //     ->limit(5)
-        
+
         $routes = Route::whereHas('favoritePoints', function ($query) use ($passengerId) {
             $query->where('passenger_id', $passengerId);
         })->with(['interest_point_starting', 'interest_point_ending', 'favoritePoints'])->get();
