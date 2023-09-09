2b7e45df094226aa8981f393c57082547da8da57
Abdalrahman-emad-saob <81173173+Abdalrahman-emad-saob@users.noreply.github.com>
2023-09-09 18:53:32 +0300

    .

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,12 +1,59 @@
-let map;
+// let map;
+// let marker;
 
-async function initMap() {
-  const { Map } = await google.maps.importLibrary("maps");
+// const { Map } = await google.maps.importLibrary("maps");
+// const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
-  map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
-}
+// async function initMap() {
+//     const position = { lat: 37.9922, lng: -1.1307 }
 
+//     let mapOptions = {
+//         zoom: 7,
+//         center: position,
+//         mapId: "DEMO_MAP_ID",
+//     };
+//     map = new Map(document.getElementById("map"), mapOptions);
+
+//     // marker = new AdvancedMarkerElement({
+//     //     map: map,
+//     //     position: position,
+//     // });
+// }
+
+// initMap();
+
+// function addMarker(location){
+//     marker = new AdvancedMarkerElement({
+//         map: map,
+//         position: location,
+//     });
+// }
+
+// addMarker({ lat: 37.9922, lng: -1.1307 })
+// addMarker({lat: 39.4699, lng: -0.3763})
+
+
+    function initMap() {
+        var directionsService = new google.maps.DirectionsService();
+        var directionsRenderer = new google.maps.DirectionsRenderer();
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 10,
+            center: {lat: 232, lng: -1.021}, // Change the center to your desired location
+        });
+        directionsRenderer.setMap(map);
+
+        var request = {
+            origin: 'San Francisco, CA', // Starting point
+            destination: 'Los Angeles, CA', // Destination
+            travelMode: 'DRIVING' // You can use BICYCLING, TRANSIT, or WALKING as well
+        };
+
+        directionsService.route(request, function (result, status) {
+            if (status == 'OK') {
+                directionsRenderer.setDirections(result);
+            }
+        });
+    }
+    // Initialize the map when the page loads
+    // google.maps.event.addDomListener(window, 'load', initMap);
 initMap();
--- resources/views/createRoute.blade.php
+++ resources/views/createRoute.blade.php
@@ -1,56 +1,21 @@
 <!DOCTYPE html>
 <html>
-  <head>
+
+<head>
     <title>Simple Map</title>
-    {{-- <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script> --}}
-    <script async
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyABxKvMF0OozJ23f-vFZAGxcgql_B4ghdI&callback=initMap">
-</script>
-<style>
-    /*
- * Always set the map height explicitly to define the size of the div element
- * that contains the map.
- */
- #map {
-    height: 100%;
-  }
+    @vite('resources/js/index.js')
+    <link rel="stylesheet" type="text/css" href={{ asset('css/style.css') }} />
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyABxKvMF0OozJ23f-vFZAGxcgql_B4ghdI&libraries=places"></script>
+</head>
 
-  /*
-   * Optional: Makes the sample page fill the window.
-   */
-  html,
-  body {
-    height: 100%;
-    margin: 0;
-    padding: 0;
-  }
+<body>
 
-</style>
-<script>
-    let map;
-
-async function initMap() {
-  const { Map } = await google.maps.importLibrary("maps");
-
-  map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
-}
-
-initMap();
-
-</script>
-
-    {{-- <link rel="stylesheet" type="text/css" href="./resources/css/style.css" /> --}}
-    {{-- <script type="module" src="./index.js"></script> --}}
-  </head>
-  <body>
-    <div id="map" style="width: 100%; height:100%;"></div>
-
-    <!-- prettier-ignore -->
+   <input type="text" id = "lat">
+   <input type="text" id = "lng">
+   <button id="click">Change Marker</button>
+    <div id="map"></div>
     {{-- <script>(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
-        ({key: "AIzaSyABxKvMF0OozJ23f-vFZAGxcgql_B4ghdI", v: "weekly"});</script>
-  </body> --}}
-</html>
+        ({key: "AIzaSyABxKvMF0OozJ23f-vFZAGxcgql_B4ghdI", v: "beta"});</script> --}}
+</body>
 
+</html>
